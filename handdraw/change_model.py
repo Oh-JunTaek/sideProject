dbda2bbbff3082266642a37009da6d3f39f1806e
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2024-10-02 16:54:47 +0900

    Delete change_model.py

--- handdraw/change_model.py
+++
@@ -1,7 +0,0 @@
-from tensorflow.keras.models import load_model
-
-# 기존 .h5 형식 모델 불러오기
-model = load_model('handwritten_digit_classifier.h5')
-
-# 새로운 Keras 형식으로 저장
-model.save('handwritten_digit_classifier.keras')
